c64f129e44ea37aae11a8b51f02007ecf334aac8
Xiao Yindong <xydarcher@uestc.edu.cn>
2022-03-11 22:46:26 +0800

    remove unused FifoCC.

--- lib/src/main/scala/spinal/lib/StreamFifo.scala
+++ lib/src/main/scala/spinal/lib/StreamFifo.scala
@@ -17,36 +17,36 @@ object StreamFifo {
 class StreamFifo[T <: Data](dataType: HardType[T], depth: Int) extends Component {
   require(depth >= 0)
   val io = new Bundle {
-    val push         = slave Stream (dataType)
-    val pop          = master Stream (dataType)
-    val flush        = in Bool () default (False)
-    val occupancy    = out UInt (log2Up(depth + 1) bits)
+    val push = slave Stream (dataType)
+    val pop = master Stream (dataType)
+    val flush = in Bool () default (False)
+    val occupancy = out UInt (log2Up(depth + 1) bits)
     val availability = out UInt (log2Up(depth + 1) bits)
   }
 
   val bypass = (depth == 0) generate new Area {
     io.push >> io.pop
-    io.occupancy    := 0
+    io.occupancy := 0
     io.availability := 0
   }
   val oneStage = (depth == 1) generate new Area {
     io.push.m2sPipe(flush = io.flush) >> io.pop
-    io.occupancy    := U(io.pop.valid)
+    io.occupancy := U(io.pop.valid)
     io.availability := U(!io.pop.valid)
   }
   val logic = (depth > 1) generate new Area {
-    val ram             = Mem(dataType, depth)
-    val pushPtr         = Counter(depth)
-    val popPtr          = Counter(depth)
-    val ptrMatch        = pushPtr === popPtr
+    val ram = Mem(dataType, depth)
+    val pushPtr = Counter(depth)
+    val popPtr = Counter(depth)
+    val ptrMatch = pushPtr === popPtr
     val risingOccupancy = RegInit(False)
-    val pushing         = io.push.fire
-    val popping         = io.pop.fire
-    val empty           = ptrMatch & !risingOccupancy
-    val full            = ptrMatch & risingOccupancy
+    val pushing = io.push.fire
+    val popping = io.pop.fire
+    val empty = ptrMatch & !risingOccupancy
+    val full = ptrMatch & risingOccupancy
 
-    io.push.ready  := !full
-    io.pop.valid   := !empty & !(RegNext(popPtr.valueNext === pushPtr, False) & !full) // mem write to read propagation
+    io.push.ready := !full
+    io.pop.valid := !empty & !(RegNext(popPtr.valueNext === pushPtr, False) & !full) // mem write to read propagation
     io.pop.payload := ram.readSync(popPtr.valueNext)
 
     when(pushing =/= popping) {
@@ -62,14 +62,14 @@ class StreamFifo[T <: Data](dataType: HardType[T], depth: Int) extends Component
 
     val ptrDif = pushPtr - popPtr
     if (isPow2(depth)) {
-      io.occupancy    := ((risingOccupancy && ptrMatch) ## ptrDif).asUInt
+      io.occupancy := ((risingOccupancy && ptrMatch) ## ptrDif).asUInt
       io.availability := ((!risingOccupancy && ptrMatch) ## (popPtr - pushPtr)).asUInt
     } else {
       when(ptrMatch) {
-        io.occupancy    := Mux(risingOccupancy, U(depth), U(0))
+        io.occupancy := Mux(risingOccupancy, U(depth), U(0))
         io.availability := Mux(risingOccupancy, U(0), U(depth))
       } otherwise {
-        io.occupancy    := Mux(pushPtr > popPtr, ptrDif, U(depth) + ptrDif)
+        io.occupancy := Mux(pushPtr > popPtr, ptrDif, U(depth) + ptrDif)
         io.availability := Mux(pushPtr > popPtr, U(depth) + (popPtr - pushPtr), (popPtr - pushPtr))
       }
     }
@@ -94,21 +94,21 @@ class StreamFifoLowLatency[T <: Data](
 ) extends Component {
   require(depth >= 1)
   val io = new Bundle with StreamFifoInterface[T] {
-    val push                         = slave Stream (dataType)
-    val pop                          = master Stream (dataType)
-    val flush                        = in Bool () default (False)
-    val occupancy                    = out UInt (log2Up(depth + 1) bit)
+    val push = slave Stream (dataType)
+    val pop = master Stream (dataType)
+    val flush = in Bool () default (False)
+    val occupancy = out UInt (log2Up(depth + 1) bit)
     override def pushOccupancy: UInt = occupancy
-    override def popOccupancy: UInt  = occupancy
+    override def popOccupancy: UInt = occupancy
   }
-  val vec             = useVec generate Vec(Reg(dataType), depth)
-  val ram             = !useVec generate Mem(dataType, depth)
-  val pushPtr         = Counter(depth)
-  val popPtr          = Counter(depth)
-  val ptrMatch        = pushPtr === popPtr
+  val vec = useVec generate Vec(Reg(dataType), depth)
+  val ram = !useVec generate Mem(dataType, depth)
+  val pushPtr = Counter(depth)
+  val popPtr = Counter(depth)
+  val ptrMatch = pushPtr === popPtr
   val risingOccupancy = RegInit(False)
-  val empty           = ptrMatch & !risingOccupancy
-  val full            = ptrMatch & risingOccupancy
+  val empty = ptrMatch & !risingOccupancy
+  val full = ptrMatch & risingOccupancy
 
   val pushing = io.push.fire
   val popping = io.pop.fire
@@ -120,15 +120,15 @@ class StreamFifoLowLatency[T <: Data](
   latency match {
     case 0 => {
       when(!empty) {
-        io.pop.valid   := True
+        io.pop.valid := True
         io.pop.payload := readed
       } otherwise {
-        io.pop.valid   := io.push.valid
+        io.pop.valid := io.push.valid
         io.pop.payload := io.push.payload
       }
     }
     case 1 => {
-      io.pop.valid   := !empty
+      io.pop.valid := !empty
       io.pop.payload := readed
     }
   }
@@ -169,62 +169,6 @@ object StreamFifoCC {
     new StreamFifoCC(dataType, depth, pushClock, popClock)
 }
 
-//class   StreamFifoCC[T <: Data](dataType: HardType[T], val depth: Int, val pushClock: ClockDomain,val popClock: ClockDomain) extends Component {
-//
-//  assert(isPow2(depth) & depth >= 2, "The depth of the StreamFifoCC must be a power of 2 and equal or bigger than 2")
-//
-//  val io = new Bundle with StreamFifoInterface[T]{
-//    val push          = slave  Stream(dataType)
-//    val pop           = master Stream(dataType)
-//    val pushOccupancy = out UInt(log2Up(depth + 1) bits)
-//    val popOccupancy  = out UInt(log2Up(depth + 1) bits)
-//  }
-//
-//  val ptrWidth = log2Up(depth) + 1
-//  def isFull(a: Bits, b: Bits) = a(ptrWidth - 1 downto ptrWidth - 2) === ~b(ptrWidth - 1 downto ptrWidth - 2) && a(ptrWidth - 3 downto 0) === b(ptrWidth - 3 downto 0)
-//  def isEmpty(a: Bits, b: Bits) = a === b
-//
-//  val ram = Mem(dataType, depth)
-//
-//  val popToPushGray = Bits(ptrWidth bits)
-//  val pushToPopGray = Bits(ptrWidth bits)
-//
-//  val pushCC = new ClockingArea(pushClock) {
-//    val pushPtr     = Counter(depth << 1)
-//    val pushPtrGray = RegNext(toGray(pushPtr.valueNext)) init(0)
-//    val popPtrGray  = BufferCC(popToPushGray, B(0, ptrWidth bits))
-//    val full        = isFull(pushPtrGray, popPtrGray)
-//
-//    io.push.ready := !full
-//
-//    when(io.push.fire) {
-//      ram(pushPtr.resized) := io.push.payload
-//      pushPtr.increment()
-//    }
-//
-//    io.pushOccupancy := (pushPtr - fromGray(popPtrGray)).resized
-//  }
-//
-//  val popCC = new ClockingArea(popClock) {
-//    val popPtr      = Counter(depth << 1)
-//    val popPtrGray  = RegNext(toGray(popPtr.valueNext)) init(0)
-//    val pushPtrGray = BufferCC(pushToPopGray, B(0, ptrWidth bit))
-//    val empty       = isEmpty(popPtrGray, pushPtrGray)
-//
-//    io.pop.valid   := !empty
-//    io.pop.payload := ram.readSync(popPtr.valueNext.resized, clockCrossing = true)
-//
-//    when(io.pop.fire) {
-//      popPtr.increment()
-//    }
-//
-//    io.popOccupancy := (fromGray(pushPtrGray) - popPtr).resized
-//  }
-//
-//  pushToPopGray := pushCC.pushPtrGray
-//  popToPushGray := popCC.popPtrGray
-//}
-
 class StreamFifoCC[T <: Data](
     val dataType: HardType[T],
     val depth: Int,
@@ -236,10 +180,10 @@ class StreamFifoCC[T <: Data](
   assert(isPow2(depth) & depth >= 2, "The depth of the StreamFifoCC must be a power of 2 and equal or bigger than 2")
 
   val io = new Bundle with StreamFifoInterface[T] {
-    val push          = slave Stream (dataType)
-    val pop           = master Stream (dataType)
+    val push = slave Stream (dataType)
+    val pop = master Stream (dataType)
     val pushOccupancy = out UInt (log2Up(depth + 1) bits)
-    val popOccupancy  = out UInt (log2Up(depth + 1) bits)
+    val popOccupancy = out UInt (log2Up(depth + 1) bits)
   }
 
   val ptrWidth = log2Up(depth) + 1
@@ -254,17 +198,17 @@ class StreamFifoCC[T <: Data](
   val pushToPopGray = Bits(ptrWidth bits)
 
   val pushCC = new ClockingArea(pushClock) {
-    val pushPtr     = Reg(UInt(log2Up(2 * depth) bits)) init (0)
+    val pushPtr = Reg(UInt(log2Up(2 * depth) bits)) init (0)
     val pushPtrPlus = pushPtr + 1
     val pushPtrGray = RegNextWhen(toGray(pushPtrPlus), io.push.fire) init (0)
-    val popPtrGray  = BufferCC(popToPushGray, B(0, ptrWidth bits))
-    val full        = isFull(pushPtrGray, popPtrGray)
+    val popPtrGray = BufferCC(popToPushGray, B(0, ptrWidth bits))
+    val full = isFull(pushPtrGray, popPtrGray)
 
     io.push.ready := !full
 
     when(io.push.fire) {
       ram(pushPtr.resized) := io.push.payload
-      pushPtr              := pushPtrPlus
+      pushPtr := pushPtrPlus
     }
 
     io.pushOccupancy := (pushPtr - fromGray(popPtrGray)).resized
@@ -272,13 +216,13 @@ class StreamFifoCC[T <: Data](
 
   val finalPopCd = popClock.withOptionalBufferedResetFrom(withPopBufferedReset)(pushClock)
   val popCC = new ClockingArea(finalPopCd) {
-    val popPtr      = Reg(UInt(log2Up(2 * depth) bits)) init (0)
-    val popPtrPlus  = popPtr + 1
-    val popPtrGray  = RegNextWhen(toGray(popPtrPlus), io.pop.fire) init (0)
+    val popPtr = Reg(UInt(log2Up(2 * depth) bits)) init (0)
+    val popPtrPlus = popPtr + 1
+    val popPtrGray = RegNextWhen(toGray(popPtrPlus), io.pop.fire) init (0)
     val pushPtrGray = BufferCC(pushToPopGray, B(0, ptrWidth bit))
-    val empty       = isEmpty(popPtrGray, pushPtrGray)
+    val empty = isEmpty(popPtrGray, pushPtrGray)
 
-    io.pop.valid   := !empty
+    io.pop.valid := !empty
     io.pop.payload := ram.readSync((io.pop.fire ? popPtrPlus | popPtr).resized, clockCrossing = true)
 
     when(io.pop.fire) {
@@ -296,8 +240,8 @@ case class StreamFifoMultiChannelPush[T <: Data](payloadType: HardType[T], chann
     extends Bundle
     with IMasterSlave {
   val channel = Bits(channelCount bits)
-  val full    = Bool()
-  val stream  = Stream(payloadType)
+  val full = Bool()
+  val stream = Stream(payloadType)
 
   override def asMaster(): Unit = {
     out(channel)
@@ -310,8 +254,8 @@ case class StreamFifoMultiChannelPop[T <: Data](payloadType: HardType[T], channe
     extends Bundle
     with IMasterSlave {
   val channel = Bits(channelCount bits)
-  val empty   = Bits(channelCount bits)
-  val stream  = Stream(payloadType)
+  val empty = Bits(channelCount bits)
+  val stream = Stream(payloadType)
 
   override def asMaster(): Unit = {
     out(channel)
@@ -324,11 +268,11 @@ case class StreamFifoMultiChannelPop[T <: Data](payloadType: HardType[T], channe
     (bufferOut, bufferIn).zipped.foreach((s, m) => if (withCombinatorialBuffer) s </< m else s <-< m)
 
     val needRefill = B(bufferIn.map(_.ready))
-    val selOh      = OHMasking.first(needRefill & ~empty) // TODO
-    val nonEmpty   = (~empty).orR
+    val selOh = OHMasking.first(needRefill & ~empty) // TODO
+    val nonEmpty = (~empty).orR
     channel := selOh
     for ((feed, sel) <- (bufferIn, selOh.asBools).zipped) {
-      feed.valid   := sel && nonEmpty
+      feed.valid := sel && nonEmpty
       feed.payload := stream.payload
     }
     stream.ready := (selOh & B(bufferIn.map(_.ready))).orR
@@ -346,32 +290,32 @@ case class StreamFifoMultiChannelSharedSpace[T <: Data](
 ) extends Component {
   assert(isPow2(depth))
   val io = new Bundle {
-    val push         = slave(StreamFifoMultiChannelPush(payloadType, channelCount))
-    val pop          = slave(StreamFifoMultiChannelPop(payloadType, channelCount))
+    val push = slave(StreamFifoMultiChannelPush(payloadType, channelCount))
+    val pop = slave(StreamFifoMultiChannelPop(payloadType, channelCount))
     val availability = out UInt (log2Up(depth) + 1 bits)
   }
   val ptrWidth = log2Up(depth)
 
   val payloadRam = Mem(payloadType(), depth)
-  val nextRam    = Mem(UInt(ptrWidth bits), depth)
+  val nextRam = Mem(UInt(ptrWidth bits), depth)
 
   val full = False
-  io.push.full         := full
+  io.push.full := full
   io.push.stream.ready := !full
 
   val pushNextEntry = UInt(ptrWidth bits)
-  val popNextEntry  = nextRam.wordType()
+  val popNextEntry = nextRam.wordType()
 
   val channels = for (channelId <- 0 until channelCount) yield new Area {
-    val valid    = RegInit(False)
-    val headPtr  = Reg(UInt(ptrWidth bits))
-    val lastPtr  = Reg(UInt(ptrWidth bits))
+    val valid = RegInit(False)
+    val headPtr = Reg(UInt(ptrWidth bits))
+    val lastPtr = Reg(UInt(ptrWidth bits))
     val lastFire = False
     when(io.pop.stream.fire && io.pop.channel(channelId)) {
       headPtr := popNextEntry
       when(headPtr === lastPtr) {
         lastFire := True
-        valid    := False
+        valid := False
       }
     }
 
@@ -381,7 +325,7 @@ case class StreamFifoMultiChannelSharedSpace[T <: Data](
 
     when(io.push.stream.fire && io.push.channel(channelId)) {
       lastPtr := pushNextEntry
-      valid   := True
+      valid := True
     }
     io.pop.empty(channelId) := !valid
   }
@@ -398,9 +342,9 @@ case class StreamFifoMultiChannelSharedSpace[T <: Data](
 
   val popLogic = new Area {
     val readAddress = channels.map(_.headPtr).read(OHToUInt(io.pop.channel))
-    io.pop.stream.valid   := (io.pop.channel & ~io.pop.empty).orR
+    io.pop.stream.valid := (io.pop.channel & ~io.pop.empty).orR
     io.pop.stream.payload := payloadRam.readAsync(readAddress)
-    popNextEntry          := nextRam.readAsync(readAddress)
+    popNextEntry := nextRam.readAsync(readAddress)
   }
 
   val allocationByCounter = !withAllocationFifo generate new Area {
@@ -412,7 +356,7 @@ case class StreamFifoMultiChannelSharedSpace[T <: Data](
 
     val onChannels = for (c <- channels) yield new Area {
       full setWhen (c.valid && allocationPtr === c.headPtr)
-      val wasValid     = RegNext(c.valid) init (False)
+      val wasValid = RegNext(c.valid) init (False)
       val availability = RegNext(c.headPtr - allocationPtr)
     }
 
@@ -433,33 +377,33 @@ case class StreamFifoMultiChannelSharedSpace[T <: Data](
 object StreamFifoMultiChannelBench extends App {
   val payloadType = HardType(Bits(8 bits))
   class BenchFpga(channelCount: Int) extends Rtl {
-    override def getName(): String    = "Bench" + channelCount
+    override def getName(): String = "Bench" + channelCount
     override def getRtlPath(): String = getName() + ".v"
     SpinalVerilog(new Component {
       val push = slave(StreamFifoMultiChannelPush(payloadType, channelCount))
-      val pop  = slave(StreamFifoMultiChannelPop(payloadType, channelCount))
+      val pop = slave(StreamFifoMultiChannelPop(payloadType, channelCount))
       val fifo = StreamFifoMultiChannelSharedSpace(payloadType, channelCount, 32)
 
       fifo.io.push.channel := RegNext(push.channel)
-      push.full            := RegNext(fifo.io.push.full)
+      push.full := RegNext(fifo.io.push.full)
       fifo.io.push.stream <-/< push.stream
 
       fifo.io.pop.channel := RegNext(pop.channel)
-      pop.empty           := RegNext(fifo.io.pop.empty)
+      pop.empty := RegNext(fifo.io.pop.empty)
       pop.stream <-/< fifo.io.pop.stream
 
       setDefinitionName(BenchFpga.this.getName())
     })
   }
   class BenchFpga2(channelCount: Int) extends Rtl {
-    override def getName(): String    = "BenchToStream" + channelCount
+    override def getName(): String = "BenchToStream" + channelCount
     override def getRtlPath(): String = getName() + ".v"
     SpinalVerilog(new Component {
       val push = slave(StreamFifoMultiChannelPush(payloadType, channelCount))
       val fifo = StreamFifoMultiChannelSharedSpace(payloadType, channelCount, 32)
 
       fifo.io.push.channel := RegNext(push.channel)
-      push.full            := RegNext(fifo.io.push.full)
+      push.full := RegNext(fifo.io.push.full)
       fifo.io.push.stream <-/< push.stream
 
       setDefinitionName(BenchFpga2.this.getName())
