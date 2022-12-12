bf6d147c947f47cf2ce863e144bdae36ec53c99e
Xiao Yindong <xydarcher@uestc.edu.cn>
2022-12-12 21:11:26 +0800

    fix the report of "C preprocessor "/lib/cpp" fails sanity check".

--- tools.sh
+++ tools.sh
@@ -27,7 +27,7 @@ install_ghdl(){
 }
 
 install_iverilog(){
-  sudo apt install -y gperf readline-common bison flex libfl-dev
+  sudo apt install -y gperf readline-common g++ bison flex libfl-dev
   curl -fsSL https://github.com/steveicarus/iverilog/archive/v11_0.tar.gz | tar -xvz
   cd iverilog-11_0
   autoconf
